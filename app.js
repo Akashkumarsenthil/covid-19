4d154cabdb2de6e8f9c781a7fd6e1e70fddbef52
akash <akashkumarsenthil@gmail.com>
2020-04-23 15:32:50 +0530

    bootstrap-update

--- app.js
+++ app.js
@@ -3,26 +3,64 @@ window.onload = function() {
 }
 
 function getCovidStats() {
-	fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/131')
+	fetch('https://api.covid19india.org/state_district_wise.json')
 	.then(function(resp) { return resp.json() })
-	.then(function(data) {
-		let population = data.location.country_population;
-		let update = data.location.last_updated;
-		let confirmedCases = data.location.latest.confirmed;
-		let deaths = data.location.latest.deaths;
-
-		document.getElementById('population').innerHTML = population.toLocaleString('en');
-		document.getElementById('update').innerHTML = update.substr(0, 10);
-		document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
-		document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
-		document.getElementById('percent').innerHTML = ((Number(deaths)/Number(confirmedCases))*100).toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2}) + "%";
-
-
-
-
+	.then(function(obj) {
+		var sc = 0
+		var dst = 0
+		for(const state in obj) {
+			sc += 1;
+			document.getElementById('card').innerHTML += 
+			`
+					<div class="container">
+						<div id = "accordian">
+							<div class="card">
+								<div class="card-header">
+									<h3>
+										<a href="#state${sc}" data-parent="#accordion" data-toggle="collapse">
+											${state}
+										</a>
+									</h3>
+								</div>
+							</div>
+						</div>
+					</div>
+			`
+			for (const item in obj[state]) {
+				if(item == 'districtData') {
+					for (const dist in obj[state][item]) {
+						document.getElementById('card').innerHTML += 
+						`	
+								<div id = "#state${sc}" class="card-header">
+									<h5>
+										<a class="text-info" href="#district${dst}" data-parent="#accordion" data-toggle="collapse">
+											${dist}
+										</a>
+									</h5>
+								</div>
+						`
+						document.getElementById('card').innerHTML += 
+						`	
+									<div id="district${dst}" class="collapse">
+										<div class="card card-body">
+											<p class="display-5">
+												Total Cases: ${obj[state][item][dist].confirmed}<br>
+												Active: ${obj[state][item][dist].active}<br> 
+												Died: ${obj[state][item][dist].deceased}<br>
+											</p>
+										</div>
+									</div>
+									
+						`
+						dst += 1
+					}
+				}
+			}
+		}
 	})
 	.catch(function() {
 		console.log("error");
 	})
 	setTimeout(getCovidStats, 43200000) // update every 12 hours
 }
+		
